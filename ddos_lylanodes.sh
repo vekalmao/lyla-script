4989a1dfaa08f5474f51d118dc2a96ae69666919
vekaomg <146497829+vekalmao@users.noreply.github.com>
2024-06-12 21:10:41 -0400

    Update ddos_lylanodes.sh

--- ddos_lylanodes.sh
+++ ddos_lylanodes.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
 
-DIR="/etc/lylanodes-protection"
-
-if [ -d "$DIR" ]; then
-    echo "Directory $DIR already exists."
+if [ -d "/etc/ddos-guardian" ]; then
+    echo "Directory /etc/ddos-guardian is already installed..."
     exit 1
 fi
 
-mkdir -p $DIR
+confirm_installation() {
+    local answer
+    read -p "Are you sure you want to install DDoS Guardian? (yes/no): " answer </dev/tty
+    answer=${answer,,}
+    answer=${answer:-no}
+    if [ "$answer" = "yes" ] || [ "$answer" = "y" ]; then
+        echo "Installing DDoS Guardian..."
+        install_ddos_guardian
+    else
+        echo "Installation canceled."
+        exit 1
+    fi
+}
 
-cd $DIR
+install_ddos_guardian() {
+    
+    cd /etc/
+    
+    apt update
 
-git clone https://github.com/vekalmao/lyla-script/ .
-
-if ! command -v node &> /dev/null; then
-    curl -sL https://deb.nodesource.com/setup_14.x | bash -
-    apt install -y nodejs
-fi
-
-apt update
-apt upgrade -y
-
-# Install required Node.js modules
-npm install express
-npm install http-proxy
-npm install fs
-npm install net
-npm install dgram
-npm install express-rate-limit
-npm install child_process
-npm install url
-
-cat <<EOF > /etc/systemd/system/lylanodes.service
+    mkdir ddos-guardian
+    cd ddos-guardian
+    
+    curl -Lo ddos-guardian.tar.gz https://github.com/DDOS-Guardian/DDoS-Guardian/releases/latest/download/ddos-guardian.tar.gz
+    tar -xvzf ddos-guardian.tar.gz
+    rm ddos-guardian.tar.gz
+    
+    if ! command -v node &> /dev/null; then
+        echo "Please install NodeJS!"
+        exit 1
+    fi
+    
+    npm install
+    
+cat <<EOF > /etc/systemd/system/guardian.service
 [Unit]
-Description=LylaNodes DDos Protection
+Description=DDoS Guardian Service
 After=network.target
 
 [Service]
 Type=simple
 User=root
-WorkingDirectory=$DIR
-ExecStart=/usr/bin/node $DIR/attacks.js
+WorkingDirectory=/etc/ddos-guardian
+ExecStart=/usr/bin/node /etc/ddos-guardian/attacks.js
 Restart=always
 StandardOutput=syslog
 StandardError=syslog
@@ -49,69 +57,53 @@ StandardError=syslog
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-systemctl enable lylanodes
-systemctl start lylanodes
+    systemctl daemon-reload
+    
+    systemctl enable guardian
+    systemctl start guardian
+    
+    
+    iptables -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+    iptables -A INPUT -p udp -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+    iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+    iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+    iptables -A INPUT -j DROP
+    
+    iptables -A INPUT -i lo -j ACCEPT
+    
+    
+    iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+    
+    
+    iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+    iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+    
+    
+    iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+    
+    
+    iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+    iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+    
+    
+    iptables -A INPUT -p icmp -m limit --limit 1/s -j ACCEPT
+    
+    iptables -A INPUT -j LOG --log-prefix "Dropped: "
+    
+    
+    iptables -A INPUT -j DROP
+    
+    
+    iptables-save > /etc/iptables/rules.v4
+    
+    
+    cd /etc/nginx/conf.d/
 
-iptables-save > /etc/iptables/rules.backup
+    curl -Lo protection.lua https://raw.githubusercontent.com/DDOS-Guardian/DDoS-Guardian-Layer-7/main/protection.lua
+    
+    sudo apt-get install libnginx-mod-http-lua
+    
+    echo "DDoS Guardian setup complete."
+}
 
-cat <<EOF > /etc/lylanodes-protection/firewall.sh
-#!/bin/bash
-
-iptables -F
-iptables -X
-
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
-
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
-
-# Allow HTTP and HTTPS connections
-iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT
-
-# Allow ports 8080 and 2022
-iptables -A INPUT -p tcp --dport 8080 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A INPUT -p tcp --dport 2022 -m conntrack --ctstate NEW -j ACCEPT
-
-iptables -A INPUT -p tcp --syn -m connlimit --connlimit-above 10 -j REJECT
-
-iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 3 -j ACCEPT
-
-iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
-
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
-
-iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
-
-iptables -A INPUT -j LOG --log-prefix "iptables: " --log-level 4
-
-iptables -A OUTPUT -j ACCEPT
-
-iptables-save > /etc/iptables/rules.v4
-EOF
-
-chmod +x /etc/lylanodes-protection/firewall.sh
-
-/etc/lylanodes-protection/firewall.sh
-
-sysctl -w net.ipv4.ip_forward=0
-sysctl -w net.ipv4.tcp_syncookies=1
-sysctl -w net.ipv4.conf.all.accept_source_route=0
-sysctl -w net.ipv6.conf.all.accept_source_route=0
-sysctl -w net.ipv4.conf.all.accept_redirects=0
-sysctl -w net.ipv6.conf.all.accept_redirects=0
-sysctl -w net.ipv4.conf.all.rp_filter=1
-sysctl -w net.ipv4.conf.all.log_martians=1
-
-sysctl -p
-
-echo -e "\033[0;32m[ LylaNodes DDos Protection Script Works! ]\033[0m"
-echo -e "\033[0;32m[ Made by LylaNodes Hosting ] !\033[0m"
-echo -e "\033[0;32m[ Use systemctl status lylanodes, to see if it's running. ] !\033[0m"
+confirm_installation
